20fe5c4eed2cdb4eb6445ff6d543b5b07e4663fb
= <skaad102@gmail.com>
2025-02-05 13:20:05 -0500

    readme and test

--- README.md
+++ README.md
@@ -4,9 +4,18 @@ A new Flutter project.
 
 ## INFO PROJECT
 
-- Flutter 2.24.1
-- Dart 2.12.0
+- Flutter 3.27.1
+- Dart 3.6.1
 
 ## INFO DEPENDENCIES DEV
 
 - stacked_generator 1.3.3 [link](https://stacked.filledstacks.com/docs/getting-started/overview)
+
+
+## Run Project
+
+```flutter pub get```
+
+```stacked generate``` 
+
+```flutter run```
--- test/services/posts_service_test.dart
+++ test/services/posts_service_test.dart
@@ -1,11 +1,42 @@
+import 'package:ceiba_app/services/posts_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ceiba_app/app/app.locator.dart';
+import 'package:mockito/mockito.dart';
 
 import '../helpers/test_helpers.dart';
+import '../helpers/test_helpers.mocks.dart';
 
 void main() {
+  late PostsService postsService;
+  late MockBackendApiService mockBackendApiService;
+
+  setUp(() {
+    mockBackendApiService = getAndRegisterBackendApiService();
+    postsService = PostsService();
+  });
   group('PostsServiceTest -', () {
-    setUp(() => registerServices());
     tearDown(() => locator.reset());
+
+    test('When fetchPosts is called, should call backendApiService.get',
+        () async {
+      // Arrange
+      when(mockBackendApiService
+              .get(postPath, queryParameters: {'userId': '1'}))
+          .thenAnswer((_) async => [
+                {
+                  "userId": 1,
+                  "id": 1,
+                  "title":
+                      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                  "body":
+                      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestias ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                },
+              ]);
+      // Act
+      await postsService.fetchPostsByUserId('1');
+      // Assert
+      verify(mockBackendApiService
+          .get(postPath, queryParameters: {'userId': '1'})).called(1);
+    });
   });
 }
